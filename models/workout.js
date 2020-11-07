059849642afad5939c42ebab0c14f90808e63ce3
Liam Bradley <liamhbradley11@gmail.com>
2020-11-06 16:21:53 -0800

    workout.js functionality

---
+++ models/workout.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+      day: {
+        type: Date,
+        default: () => new Date()
+      },
+      exercises: [
+        {
+          type: {
+            type: String,
+            trim: true,
+            required: "Exercise type?"
+          },
+          name: {
+            type: String,
+            trim: true,
+            required: "Exercise name?"
+          },
+          duration: {
+            type: Number,
+            required: "How long?"
+          },
+          weight: {
+            type: Number
+          },
+          reps: {
+            type: Number
+          },
+          sets: {
+            type: Number
+          },
+          distance: {
+            type: Number
+          }
+        }
+      ]
+    },
+    {
+      toJSON: {
+
+        virtuals: true
+      }
+    }
+  );
+
+  workoutSchema.virtual("totalDuration").get(function () {
+
+    return this.exercises.reduce((total, exercise) => {
+      return total + exercise.duration;
+    }, 0);
+  });
+
+  const Workout = mongoose.model("Workout", workoutSchema);
+
+  module.exports = Workout;
